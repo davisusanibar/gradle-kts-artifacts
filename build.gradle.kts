ded4e7d705414eb6737feaa7805b30465e3d7f17
david dali susanibar arce <davi.sarces@gmail.com>
2022-10-24 11:28:25 -0500

    Test publsh packages to Github repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ allprojects {
                     url
                 }
                 credentials {
-                    username = System.getenv("GITHUB_ACTOR")
-                    password = System.getenv("GITHUB_TOKEN")
+                    username = System.getenv("USERNAME")
+                    password = System.getenv("TOKEN")
                 }
             }
         }
