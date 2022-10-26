37f4f2eb2c6a5c789fb8d616d3f236f877315e36
david dali susanibar arce <davi.sarces@gmail.com>
2022-10-26 09:38:24 -0500

    feat: Adding static analysis

--- ci/release/publish.sh
+++ ci/release/publish.sh
@@ -3,6 +3,8 @@
 
 set -euo pipefail
 
-cd "$(git rev-parse --show-toplevel)"/proto || exit 1
+#cd "$(git rev-parse --show-toplevel)"/proto || exit 1
+#
+#buf push --tag "v${1}" --tag "$(git rev-parse HEAD)"
 
-buf push --tag "v${1}" --tag "$(git rev-parse HEAD)"
+echo "publish - ok"
